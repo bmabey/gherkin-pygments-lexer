bmabey/gherkin-pygments-lexer

commit 62b5570b0af5f7897a1861f46937778fb9c797e1

fixes table bars being treated as strings

gherkin_lexer/__init__.py
@@ -40,7 +40,7 @@ class GherkinLexer(RegexLexer):
         'examples_table_header': [
             (r"\s+\|\s*$", Keyword, "#pop:2"),
             include('comments'),
-            (r"\s+\|", Keyword),
+            (r"\s.\|", Keyword),
             (r"[^\|]", Name.Variable),
           ],
         'scenario_sections_on_stack': [
@@ -64,7 +64,7 @@ class GherkinLexer(RegexLexer):
           'table_content': [
             (r"\s+\|\s*$", Keyword, "#pop"),
             include('comments'),
-            (r"\s+\|", Keyword),
+            (r"\s*\|", Keyword),
             include('string'),
           ],
         'double_string': [

lexer.erb.py
@@ -40,7 +40,7 @@ class GherkinLexer(RegexLexer):
         'examples_table_header': [
             (r"\s+\|\s*$", Keyword, "#pop:2"),
             include('comments'),
-            (r"\s+\|", Keyword),
+            (r"\s.\|", Keyword),
             (r"[^\|]", Name.Variable),
           ],
         'scenario_sections_on_stack': [
@@ -64,7 +64,7 @@ class GherkinLexer(RegexLexer):
           'table_content': [
             (r"\s+\|\s*$", Keyword, "#pop"),
             include('comments'),
-            (r"\s+\|", Keyword),
+            (r"\s*\|", Keyword),
             include('string'),
           ],
         'double_string': [

commit e762dd8d8b40052831f5176a2a12abbf49e2bfb8

reverts bug introduced in 62b5570b mislabeling example table header bars

gherkin_lexer/__init__.py
@@ -40,7 +40,7 @@ class GherkinLexer(RegexLexer):
         'examples_table_header': [
             (r"\s+\|\s*$", Keyword, "#pop:2"),
             include('comments'),
-            (r"\s.\|", Keyword),
+            (r"\s*\|", Keyword),
             (r"[^\|]", Name.Variable),
           ],
         'scenario_sections_on_stack': [

lexer.erb.py
@@ -40,7 +40,7 @@ class GherkinLexer(RegexLexer):
         'examples_table_header': [
             (r"\s+\|\s*$", Keyword, "#pop:2"),
             include('comments'),
-            (r"\s.\|", Keyword),
+            (r"\s*\|", Keyword),
             (r"[^\|]", Name.Variable),
           ],
         'scenario_sections_on_stack': [